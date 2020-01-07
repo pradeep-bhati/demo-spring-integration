6f019d329d67777b00726fc945f6de6bc0df31fe
pradeep.bhati <pradeep.bhati@MUMWX12268.internal.imsglobal.com>
2020-01-07 19:15:08 +0530

    Added transformer and service activator.

--- exercise/src/main/java/com/iqvia/exercise/InterationConfig.java
+++ exercise/src/main/java/com/iqvia/exercise/InterationConfig.java
@@ -1,20 +1,41 @@
 package com.iqvia.exercise;
 
+import java.io.File;
 import java.net.MalformedURLException;
 import java.net.URL;
 import java.util.List;
+import java.util.logging.Logger;
 import java.util.stream.Collectors;
 
-import org.aspectj.bridge.Message;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.io.Resource;
+import org.springframework.integration.annotation.ServiceActivator;
+import org.springframework.integration.annotation.Transformer;
 import org.springframework.integration.core.MessageSource;
 import org.springframework.integration.dsl.IntegrationFlow;
 import org.springframework.integration.dsl.IntegrationFlows;
+import org.springframework.integration.dsl.Pollers;
+import org.springframework.integration.endpoint.AbstractMessageSource;
+import org.springframework.integration.feed.config.FeedInboundChannelAdapterParser;
+import org.springframework.integration.feed.dsl.Feed;
+import org.springframework.integration.feed.dsl.FeedEntryMessageSourceSpec;
+import org.springframework.integration.feed.inbound.FeedEntryMessageSource;
+import org.springframework.integration.file.FileWritingMessageHandler;
+import org.springframework.integration.file.support.FileExistsMode;
+import org.springframework.integration.metadata.MetadataStore;
+import org.springframework.integration.metadata.PropertiesPersistingMetadataStore;
+import org.springframework.integration.scheduling.PollerMetadata;
 import org.springframework.integration.support.MessageBuilder;
+import org.springframework.integration.support.management.MessageSourceManagement;
+import org.springframework.messaging.Message;
+import org.springframework.messaging.MessageHandler;
+import org.springframework.integration.annotation.Poller;
 
+import com.rometools.rome.feed.module.Module;
 import com.rometools.rome.feed.synd.SyndContent;
 import com.rometools.rome.feed.synd.SyndEntry;
 import com.rometools.rome.feed.synd.SyndFeed;
@@ -25,71 +46,51 @@ import com.rometools.rome.io.XmlReader;
 @Configuration
 public class InterationConfig {
 	
-	
-	
 	@Value("${feed.url}")
-	private String feedUrl;
+	private Resource feedResource;
+
 	
-	
-	
-	@Bean
-	public MessageBuilder<SyndFeed> getMessage() throws Exception
-	{
-		URL url = new URL(feedUrl);
-		SyndFeedInput input = new SyndFeedInput();
-		SyndFeed feed = input.build(new XmlReader(url));
-		
-		
-		System.out.println("pradeep");
-//		System.out.println(feed.getAuthor());
-//		System.out.println(feed.getDocs());
-//		System.out.println(feed.getTitle());
-//		System.out.println(feed.getDescription());
-		List<SyndEntry>  str = feed.getEntries();
-		List<SyndLink> list = feed.getLinks();
-		System.out.println("single link"+feed.getLink());
-		
-//		str.stream().forEach(a -> System.out.println(a.getCategories()));
-//		List<List<SyndContent>> list = str.stream().map(a -> a.getContents()).collect(Collectors.toList());
-//		list.stream().flatMap(a -> a.)
-		System.out.println(feed.getLinks().size());
-		System.out.println(feed.getEntries().size());
-		for(SyndEntry s : str) {
-//			System.out.println("title"+s.getTitle());
-//			System.out.println("link"+s.getLink());
-//			System.out.println("description"+s.getDescription().getValue());
-//			System.out.println("date"+s.getPublishedDate().toString());
-//			System.out.println("category size"+s.getCategories().size());
-			System.out.println("category size"+s.getContributors().size());
-			
-			System.out.println("module size:"+s.getModules().size());
-			s.getCategories().stream().forEach(a -> System.out.println(a.getName()));
-			
-//			System.out.println("guid size"+s.getCategories().s);
-//			System.out.println("size of authors"+s.getAuthors().size());
-//			System.out.println("size of category"+s.getCategories().size());
-//			System.out.println("size of contributors"+s.getContributors().size());
-			//System.out.println(s.get.size());
-//			System.out.println("size of modules"+s.getModules().size());
-			//System.out.println(s.getDescription());
-//			System.out.println("size of mark up"+s.getForeignMarkup().size());
-//			System.out.println("size of closure"+s.getEnclosures().size());
-//			System.out.println("size of links"+s.getLinks().size());
-//			s.getCategories().stream().forEach(a -> System.out.println("category"+a.getName()));
-//			s.getContents().stream().forEach(a -> System.out.println(a.getValue()));
-		}
-//		str.stream().forEach(a -> System.out.println(a.getC));
-//		feed.getEntries().stream().forEach(a -> {a.getContents().stream().forEach(b -> System.out.println(b.getValue()));});
-		return MessageBuilder.withPayload(feed);
-			
-	};
-	
+	   @Bean
+	   public IntegrationFlow feedFlow() {
+	      return IntegrationFlows
+	            .from(Feed.inboundAdapter(this.feedResource, "pubDate")
+	                        .metadataStore(metadataStore()),
+	                  e -> e.poller(p -> p.fixedDelay(1000).maxMessagesPerPoll(10)))
+//	          .transform(Transformers.objectToString())
+	            .transform(m -> transformSyndEntryToString(m))
+	            .channel(c -> c.queue("entries"))
+	            .get();
+	   }
+
 	
 
-	@Bean
-	public IntegrationFlow getFlow() throws Exception
-	{
-		return IntegrationFlows.from(getMessage().getPayload().getTitle()).transform(s -> s.toString()).get();
-	}
 	
+	 @Bean
+	    public MetadataStore metadataStore() {
+	        PropertiesPersistingMetadataStore metadataStore = new PropertiesPersistingMetadataStore();
+	        metadataStore.setBaseDirectory("C:\\Users\\pradeep.bhati\\tmp\\foo");
+	        return metadataStore;
+	    }
+	 
+	   @Transformer
+	   String transformSyndEntryToString(Object o) {
+	      SyndEntry message = (SyndEntry) o;
+	      String processedMsg = message.getComments();
+	       return processedMsg;
+	   }
+	
+	    @Bean	
+	    @ServiceActivator(inputChannel = "entries", poller = @Poller(fixedRate = "5000", maxMessagesPerPoll = "10"))
+	    public MessageHandler fileWritingMessageHandler() {
+	        FileWritingMessageHandler handler = new FileWritingMessageHandler(new File("C:\\Users\\pradeep.bhati\\tmp\\processed-msgs"));
+	        handler.setFileExistsMode(FileExistsMode.REPLACE);
+	        handler.setExpectReply(false);
+	        return handler;
+	    }
+	    
+	    @Bean(name = PollerMetadata.DEFAULT_POLLER)
+	    public PollerMetadata poller() {
+	        return Pollers.fixedDelay(1000).get();
+	    }
+
 }
