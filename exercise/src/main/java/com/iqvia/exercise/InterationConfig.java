7d8abbe30dc4ca0a56c8185337a251928b7a366c
pradeep.bhati <pradeep.bhati@MUMWX12268.internal.imsglobal.com>
2020-01-10 12:26:18 +0530

    Added root Folder and logger.

--- exercise/src/main/java/com/iqvia/exercise/InterationConfig.java
+++ exercise/src/main/java/com/iqvia/exercise/InterationConfig.java
@@ -3,6 +3,7 @@ package com.iqvia.exercise;
 import java.io.File;
 import java.net.MalformedURLException;
 import java.net.URL;
+import java.text.SimpleDateFormat;
 import java.util.List;
 import java.util.logging.Logger;
 import java.util.stream.Collectors;
@@ -13,6 +14,8 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.io.Resource;
+import org.springframework.expression.Expression;
+import org.springframework.expression.spel.standard.SpelExpressionParser;
 import org.springframework.integration.annotation.ServiceActivator;
 import org.springframework.integration.annotation.Transformer;
 import org.springframework.integration.core.MessageSource;
@@ -24,8 +27,11 @@ import org.springframework.integration.feed.config.FeedInboundChannelAdapterPars
 import org.springframework.integration.feed.dsl.Feed;
 import org.springframework.integration.feed.dsl.FeedEntryMessageSourceSpec;
 import org.springframework.integration.feed.inbound.FeedEntryMessageSource;
+import org.springframework.integration.file.FileNameGenerator;
 import org.springframework.integration.file.FileWritingMessageHandler;
+import org.springframework.integration.file.dsl.Files;
 import org.springframework.integration.file.support.FileExistsMode;
+import org.springframework.integration.handler.LoggingHandler;
 import org.springframework.integration.metadata.MetadataStore;
 import org.springframework.integration.metadata.PropertiesPersistingMetadataStore;
 import org.springframework.integration.scheduling.PollerMetadata;
@@ -48,6 +54,9 @@ public class InterationConfig {
 	
 	@Value("${feed.url}")
 	private Resource feedResource;
+	
+	@Value("${root.folder}")
+	private String rootFolder;
 
 	
 	   @Bean
@@ -55,14 +64,48 @@ public class InterationConfig {
 	      return IntegrationFlows
 	            .from(Feed.inboundAdapter(this.feedResource, "pubDate")
 	                        .metadataStore(metadataStore()),
-	                  e -> e.poller(p -> p.fixedDelay(1000).maxMessagesPerPoll(10)))
+	                  e -> e.poller(p -> p.fixedDelay(100)))
 //	          .transform(Transformers.objectToString())
-	            .transform(m -> transformSyndEntryToString(m))
-	            .channel(c -> c.queue("entries"))
+	           .enrichHeaders(h -> h.headerExpression("fileName","payload.uri.toString()"))
+	           .enrichHeaders(h -> h.headerFunction("dir", m -> parseDirectory(m)))
+	           .enrichHeaders(h -> h.headerFunction("logData", m -> logData(m)))
+	           .transform(m -> transformSyndEntryToString(m))
+	           .log(LoggingHandler.Level.INFO,"TEST_LOGGER",m -> m.getHeaders().get("logData"))
+	           .handle(Files.outboundAdapter(m -> m.getHeaders().get("dir"))
+                       .fileNameGenerator(m -> m.getHeaders().get("fileName").toString()+".xml")
+                       .autoCreateDirectory(true))
+//	            .channel(c -> c.queue("entries"))
+	           
 	            .get();
 	   }
 
+	   
+	   String logData(Message<Object> m){
+		   
+	        SyndEntry message = (SyndEntry) m.getPayload();
+	        return message.getLink();
+	   }
 	
+	   String parseDirectory(Message<Object> m){
+		   
+		   LoggingHandler adapter = new LoggingHandler(LoggingHandler.Level.DEBUG);
+	        SyndEntry message = (SyndEntry) m.getPayload();
+	        if(message.getPublishedDate()!=null)
+	        {
+	        	SimpleDateFormat smpl = new SimpleDateFormat("yyyy-MM-dd");
+	        	String date = smpl.format(message.getPublishedDate());
+	        	if(message.getCategories()!=null) {
+	        		String category = message.getCategories().get(0).getName();
+	        		return rootFolder+"/"+date+"/"+category;
+	        	}
+	        	
+	        	return date;
+	        	}
+	        else
+	        	{return "pradeep";}
+//	        return message.getPublishedDate().toString().concat("/").concat("ashim");
+	        //return "pradeep/singh";
+	    }
 
 	
 	 @Bean
@@ -76,21 +119,31 @@ public class InterationConfig {
 	   String transformSyndEntryToString(Object o) {
 	      SyndEntry message = (SyndEntry) o;
 	      String processedMsg = message.getComments();
-	       return processedMsg;
+//	      message.getPublishedDate().toString();
+//	      message.getComments();
+//	       return processedMsg;
+	      
+	       return message.toString();
 	   }
 	
-	    @Bean	
-	    @ServiceActivator(inputChannel = "entries", poller = @Poller(fixedRate = "5000", maxMessagesPerPoll = "10"))
-	    public MessageHandler fileWritingMessageHandler() {
-	        FileWritingMessageHandler handler = new FileWritingMessageHandler(new File("C:\\Users\\pradeep.bhati\\tmp\\processed-msgs"));
-	        handler.setFileExistsMode(FileExistsMode.REPLACE);
-	        handler.setExpectReply(false);
-	        return handler;
-	    }
-	    
-	    @Bean(name = PollerMetadata.DEFAULT_POLLER)
-	    public PollerMetadata poller() {
-	        return Pollers.fixedDelay(1000).get();
-	    }
+//	    @Bean	
+//	    @ServiceActivator(inputChannel = "entries", poller = @Poller(fixedRate = "5000", maxMessagesPerPoll = "10"))
+//	    public MessageHandler fileWritingMessageHandler() {
+//	    	
+//	    	Expression directoryExpression = new SpelExpressionParser().parseExpression("headers.dir");
+//	        FileWritingMessageHandler handler = new FileWritingMessageHandler(directoryExpression);
+//	        handler.setFileExistsMode(FileExistsMode.REPLACE);  
+//	        handler.setExpectReply(false);
+//	        handler.setFileNameGenerator(a -> a.getHeaders().getId().toString());
+//	        handler.setAutoCreateDirectory(true);
+//	        
+//	        
+//	        return handler;
+//	    }
+//	    
+//	    @Bean(name = PollerMetadata.DEFAULT_POLLER)
+//	    public PollerMetadata poller() {
+//	        return Pollers.fixedDelay(1000).get();
+//	    }
 
 }
